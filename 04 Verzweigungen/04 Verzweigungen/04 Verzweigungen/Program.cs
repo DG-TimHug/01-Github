60ce88adbc012435aadbff8c0ae0c4c1b6aca941
Tim <tim.hug@digitecgalaxus.ch>
2025-08-11 17:24:24 +0200

    Inital Commit
    if /else not functional yet to be fixed

---
+++ 04 Verzweigungen/04 Verzweigungen/04 Verzweigungen/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.Design;
+
+namespace _04_Verzweigungen;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter 2 numbers");
+        var a = Console.ReadLine();
+        var b = Console.ReadLine();
+        Console.WriteLine("Your numbers were " + a + " and " + b);
+        int.TryParse(a, out var c);
+        int.TryParse(b, out var d);
+
+        if c = d;
+        {
+            Console.WriteLine("Hoorah");
+        }
+        else {
+            Console.WriteLine("womp womp");
+        }
+
+
+    }
+}
