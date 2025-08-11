4aa286159b147e1c4cc039dc0a8ebde0bb1c551e
Tim <tim.hug@digitecgalaxus.ch>
2025-08-11 15:54:49 +0200

    finished Questions 1-4

--- 03-Operatoren/03-Operatoren/03-Operatoren/Program.cs
+++ 03-Operatoren/03-Operatoren/03-Operatoren/Program.cs
@@ -8,6 +8,8 @@ class Program
         //Console.WriteLine("Hello, World!");
 
         // Aufgabe 1
+        
+        /*
         var a = 7;
         var b = (a + a) * 5;
         Console.WriteLine("Answer = " + b);
@@ -24,6 +26,35 @@ class Program
         var i = e % f;
         Console.WriteLine("The answer is " + result + ". " + i + " remain.");
         
+        
+        */
         //Aufgabe 3
+        // Was ist das Ergebnis der beiden Beispielen und wieso unterscheiden sich diese? Einfache Mathematik. Punkt vor strich. Bei a wird 3 + 10 (2*5) gerechnet. Bei b wird 5 (3+2) * 5 gerechnet
+        /*
+        var a = 3 + 2 * 5;   
+        var b = (3 + 2) * 5;
+        
+        Console.WriteLine(a + " and " + b);
+        */
+        
+        //Aufgabe 4
+        Console.WriteLine("Which numbers 1-10 can be divided by 3?");
+        
+        for (var num = 1; num <= 10; num++)
+        {
+            if (num % 3 != 0)
+            {
+                Console.WriteLine(" No");
+            }
+            else
+            {
+                Console.WriteLine(" yes");
+            }
+        }
+        //Aufgabe 5
+        
+        
+        
     }
+    
 }   
