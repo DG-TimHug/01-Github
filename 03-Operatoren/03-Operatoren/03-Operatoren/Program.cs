a94b51f4ff66a407612536edcc832da5ecb6f870
Tim <tim.hug@digitecgalaxus.ch>
2025-08-11 13:24:16 +0200

    Intial commit muss 02 fixen

---
+++ 03-Operatoren/03-Operatoren/03-Operatoren/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Operatoren;
+
+class Program
+{
+    // 03-Operatoren
+    public static void Main()
+    {
+        //Console.WriteLine("Hello, World!");
+        
+        // Aufgabe 1
+                var a = 7;
+                var b = (a + a) * 5;
+                Console.WriteLine("Answer = " + b);
+
+        // Aufgabe 2
+        Console.WriteLine("Give me 2 numbers that can be divided by eachother...");
+        var number1 = Console.ReadLine();
+        var number2 = Console.ReadLine();
+        Console.WriteLine("Your numbers were " + number1 + "and " + number2);
+    }
+}
