55850bb216d16496b81327ede48352f284ac2dba
Tim <tim.hug@digitecgalaxus.ch>
2025-08-11 09:34:45 +0200

    Intial Commit for Hello World

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Enter your name: ");
+string name = Console.ReadLine();
+Console.WriteLine("Your name is " + name);
+Console.WriteLine("Hello " + name + "!");
