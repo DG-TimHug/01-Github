088a6dbce4331ac3105051e65bd75432a3ee324c
Tim <tim.hug@digitecgalaxus.ch>
2025-08-11 11:13:30 +0200

    Inital commit 02 Data types and Variables

---
+++ 02 Daten Typen und Variablen/02-Daten Typen und Variablen/02-Daten Typen und Variablen/Program.cs
@@ -0,0 +1,29 @@
+﻿//Aufgabe 1/2 von Building Blocks/Learn to code/aufgaben/basics/02 Datentypen und Variablen
+
+// Console.WriteLine("Hello, World!");
+
+var age = 15;  // sets Age
+var name = "Tim"; // Sets Name
+
+var Person = name + ", "+ age;  //combines Variables
+Console.WriteLine(Person); //prints Variables age and name
+
+
+//Aufgabe 3
+// Beispiel 1:
+/*int x = 3;
+double y = x; */
+
+//Beispiel 2:
+double x = 3.2;
+int y = (int) x;       //Explicit casting; muss manuel von double auf int gewecheselt werden
+
+// Aufgabe 4 siehe Conf
+
+//Aufgabe 5
+
+const double e = 2.718281;
+
+const float PI = 3.141592f;
+var numbers = "e = " + e + ", " + "Pi = " + PI;
+Console.WriteLine(numbers);
