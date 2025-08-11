b7ca7e96b25e1219e220141ca757f79971442825
Tim <tim.hug@digitecgalaxus.ch>
2025-08-11 14:17:59 +0200

    Update Program.cs

--- 03-Operatoren/03-Operatoren/03-Operatoren/Program.cs
+++ 03-Operatoren/03-Operatoren/03-Operatoren/Program.cs
@@ -6,16 +6,24 @@ class Program
     public static void Main()
     {
         //Console.WriteLine("Hello, World!");
-        
+
         // Aufgabe 1
-                var a = 7;
-                var b = (a + a) * 5;
-                Console.WriteLine("Answer = " + b);
+        var a = 7;
+        var b = (a + a) * 5;
+        Console.WriteLine("Answer = " + b);
 
         // Aufgabe 2
         Console.WriteLine("Give me 2 numbers that can be divided by eachother...");
-        var number1 = Console.ReadLine();
-        var number2 = Console.ReadLine();
-        Console.WriteLine("Your numbers were " + number1 + "and " + number2);
+        var c = Console.ReadLine();
+        var d = Console.ReadLine();
+        Console.WriteLine("Your numbers were " + c + " and " + d);
+        int.TryParse(c, out var e);
+        int.TryParse(d, out var f);
+
+        var result = (e / f);
+        var i = e % f;
+        Console.WriteLine("The answer is " + result + ". " + i + " remain.");
+        
+        //Aufgabe 3
     }
-}
+}   
