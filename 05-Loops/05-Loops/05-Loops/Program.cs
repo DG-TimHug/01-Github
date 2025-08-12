261e6ea3dd4a4d2731541b1a3f7d28fca53a5fa9
Tim <tim.hug@digitecgalaxus.ch>
2025-08-12 12:57:30 +0200

    Intial Commit Pause wegen PR

---
+++ 05-Loops/05-Loops/05-Loops/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _05_Loops;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       //Aufgabe 1
+       var num = 1;
+        do
+        {
+            var result1 = num + num;
+            Console.WriteLine(num);
+            num++;
+
+            Console.WriteLine(result1);
+        } while (num < 10);
+
+
+    }
+}
