809017720499b8cb8d8c1307dd70681539ff2840
Tim <tim.hug@digitecgalaxus.ch>
2025-08-12 08:33:17 +0200

    Aufgaben 1/2

--- 04 Verzweigungen/04 Verzweigungen/04 Verzweigungen/Program.cs
+++ 04 Verzweigungen/04 Verzweigungen/04 Verzweigungen/Program.cs
@@ -5,22 +5,29 @@ namespace _04_Verzweigungen;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Enter 2 numbers");
+    {   //Aufgabe 1
+        Console.WriteLine("Enter 2 numbers...");
         var a = Console.ReadLine();
         var b = Console.ReadLine();
-        Console.WriteLine("Your numbers were " + a + " and " + b);
+        Console.WriteLine("Your numbers were " + a + " and " + b + ".");
         int.TryParse(a, out var c);
         int.TryParse(b, out var d);
 
-        if c = d;
-        {
-            Console.WriteLine("Hoorah");
+        if (c == d) {
+            Console.WriteLine("Your numbers are the same. Hooray!");
         }
         else {
-            Console.WriteLine("womp womp");
+            Console.WriteLine("Your numbers are different from each other. :(");
         }
-
-
+        //Aufgabe 2
+        var result = c % d;
+        if (result == 0) {
+            Console.WriteLine("Your numbers are multiples of each other.");
+        }
+        else {
+            Console.WriteLine("Your numbers are not multiples of each other.");
+            
+            
+        }
     }
 }
