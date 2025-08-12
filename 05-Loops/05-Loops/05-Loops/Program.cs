364b05a9e1263a8a395128ec3afc5fdf8c2108c7
Tim <tim.hug@digitecgalaxus.ch>
2025-08-12 14:17:32 +0200

    Aufgabe 2 und beginn Aufgabe 4

--- 05-Loops/05-Loops/05-Loops/Program.cs
+++ 05-Loops/05-Loops/05-Loops/Program.cs
@@ -5,13 +5,26 @@ class Program
     static void Main(string[] args)
     {
        //Aufgabe 1
-       var result = 0;
+    /*   var result = 0;
        for (int num = 1; num <= 10; num++)
        {
            result += num;
        }
 
        Console.WriteLine("The sum of the numbers 1-10 is " + result + ".");
+                                                                                */
+    /*    //Aufgabe 2
+        var result = 0;
+        for (int num = 1; num <= 10; num++)
+        {if (num % 2 == 0) 
+            result += num;
+        }
 
+        Console.WriteLine("The sum of all of the even numbers 1-10 is " + result + ".");
+                                                                                            */
+        //Aufgabe 4
+
+        var pyramid = "   *";
+        Console.WriteLine(pyramid);
     }
 }
