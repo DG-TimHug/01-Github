8bb1fb5504b7e9d707ac5f939b226e9acdd68480
Tim <tim.hug@digitecgalaxus.ch>
2025-08-12 10:52:48 +0200

    Complete Variable overhaul

--- 03-Operatoren/03-Operatoren/03-Operatoren/Program.cs
+++ 03-Operatoren/03-Operatoren/03-Operatoren/Program.cs
@@ -10,21 +10,21 @@ class Program
         // Aufgabe 1
         
         
-        var rndm_num = 7;                               //rndm_num weil es ist eine Random Number. Ich habe 7 als beispiel genommen jede andere Zahl geht auch
-        var rndm_num2 = (rndm_num + rndm_num) * 5;
-        Console.WriteLine("Answer = " + rndm_num2);
+        var rndmNum = 7;                               //rndm_num weil es ist eine Random Number. Ich habe 7 als beispiel genommen jede andere Zahl geht auch
+        var rndmNum2 = (rndmNum + rndmNum) * 5;
+        Console.WriteLine("Answer = " + rndmNum2);
 
         // Aufgabe 2
         Console.WriteLine("Give me 2 numbers that can be divided by eachother...");
-        var user_num1 = Console.ReadLine();
-        var user_num2 = Console.ReadLine();
-        Console.WriteLine("Your numbers were " + user_num1 + " and " + user_num1);
-        int.TryParse(user_num1, out var user_num1_trans);
-        int.TryParse(user_num2, out var user_num2_trans);
+        var userFirstNumStr = Console.ReadLine();
+        var userSecondNumStr = Console.ReadLine();
+        Console.WriteLine("Your numbers were " + userFirstNumStr + " and " + userSecondNumStr);
+        int.TryParse(userFirstNumStr, out var userFirstNum);
+        int.TryParse(userSecondNumStr, out var userSecondNum);
 
-        var result = (user_num1_trans / user_num2_trans);
-        var i = user_num1_trans % user_num2_trans;
-        Console.WriteLine("The answer is " + result + ". " + i + " remain.");
+        var result = (userFirstNum / userSecondNum);
+        var userCalcRemain = userFirstNum % userSecondNum;
+        Console.WriteLine("The answer is " + result + ". " + userCalcRemain + " remain.");
         
         
         //Aufgabe 3
