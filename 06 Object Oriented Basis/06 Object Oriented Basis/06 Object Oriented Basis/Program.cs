f0763ce75dad8f67c49258bb74fbce4a57aed775
Tim <tim.hug@digitecgalaxus.ch>
2025-08-13 09:57:24 +0200

    Initial Commit 06 Object Oriented Basis

---
+++ 06 Object Oriented Basis/06 Object Oriented Basis/06 Object Oriented Basis/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _06_Object_Oriented_Basis;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+      
+        
+        
+        
+        
+        
+        
+        
+    }
+}
