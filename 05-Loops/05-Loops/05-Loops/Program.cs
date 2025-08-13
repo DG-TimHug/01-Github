36b16af2176062f36e53fc5201fb45cf55faad52
Tim <tim.hug@digitecgalaxus.ch>
2025-08-13 09:09:29 +0200

    Aufgabe 5 Fertig
    Aufgabe 5 fertig mit comments/erklärungen. Wurde mit hilfe von Buch seite 152/153 gelöst

--- 05-Loops/05-Loops/05-Loops/Program.cs
+++ 05-Loops/05-Loops/05-Loops/Program.cs
@@ -4,68 +4,72 @@ class Program
 {
     static void Main(string[] args)
     {
-       //Aufgabe 1
-    /*   var result = 0;
-       for (int num = 1; num <= 10; num++)
-       {
-           result += num;
-       }
+        //Aufgabe 1
+        /*   var result = 0;
+           for (int num = 1; num <= 10; num++)
+           {
+               result += num;
+           }
 
-       Console.WriteLine("The sum of the numbers 1-10 is " + result + ".");
-       
-                                                                                */
-    /*    //Aufgabe 2
-        var result = 0;
-        for (int num = 1; num <= 10; num++)
-        {if (num % 2 == 0) 
-            result += num;
-        }
+           Console.WriteLine("The sum of the numbers 1-10 is " + result + ".");
 
-        Console.WriteLine("The sum of all of the even numbers 1-10 is " + result + ".");
-                                                                                            */
-        //Aufgabe 4
-    /*    
-        Console.WriteLine("For the number you enter a Pyramid that size will be shown...");
-        var userFirstNumStr = Console.ReadLine();
-        Console.WriteLine("Your pyramid size is " + userFirstNumStr + ".");
-        int.TryParse(userFirstNumStr, out var userFirstNum);
-        var pyramid = "";
-        for (int levels = 1; levels <= userFirstNum; levels++)
-        {
-            var space = "";
-            var test = (userFirstNum - levels) ;    //calculates amount of spaces needed for Stars
-            for (int i = 0; i < test; i++)
-            {
-                space += " "; //adds spaces appropriate to amount of layers of stars
+                                                                                    */
+        /*    //Aufgabe 2
+            var result = 0;
+            for (int num = 1; num <= 10; num++)
+            {if (num % 2 == 0)
+                result += num;
             }
-            pyramid += "* "; //adds star to pyramid amount of times given by user
-           
-            Console.WriteLine(space + pyramid);
-           // Console.WriteLine(test);
-                                                                                            */
-        
+
+            Console.WriteLine("The sum of all of the even numbers 1-10 is " + result + ".");
+                                                                                                */
+        //Aufgabe 4
+        /*
+            Console.WriteLine("For the number you enter a Pyramid that size will be shown...");
+            var userFirstNumStr = Console.ReadLine();
+            Console.WriteLine("Your pyramid size is " + userFirstNumStr + ".");
+            int.TryParse(userFirstNumStr, out var userFirstNum);
+            var pyramid = "";
+            for (int levels = 1; levels <= userFirstNum; levels++)
+            {
+                var space = "";
+                var test = (userFirstNum - levels) ;    //calculates amount of spaces needed for Stars
+                for (int i = 0; i < test; i++)
+                {
+                    space += " "; //adds spaces appropriate to amount of layers of stars
+                }
+                pyramid += "* "; //adds star to pyramid amount of times given by user
+
+                Console.WriteLine(space + pyramid);
+               // Console.WriteLine(test);
+                                                                                                */
+
         //Aufgabe 5
         Console.WriteLine("Quiz time: Approximately how many tunnels does Switzerland have?");
         Console.WriteLine("Is it...");
-        Console.WriteLine("A: 700 Tunnels?");
+        Console.WriteLine("A: 700 Tunnels?");                                  //Shows Users possible answers
         Console.WriteLine("B: 1000 Tunnels?");
         Console.WriteLine("C: 1300 Tunnels?");
         Console.WriteLine("D: 1500 Tunnels?");
-        Console.WriteLine("E: 1700 Tunnenls?");
-        var userInput = Console.ReadLine();
-        var a = false;
-        var b = true;
-        var c = false;
-        var d = false;
-        var e = false;
-        if (userInput == b)
-        while (userInput != true)
+        Console.WriteLine("E: 1700 Tunnels?");
+        Console.WriteLine("Please answer with the amount of tunnels.");
+        int userAnswer;                                                        //declares that userAnswer exists
+        var numbAttempts = 0;
+        if (int.TryParse(Console.ReadLine(), out userAnswer))               //If the Console can be read and converted into integer continue
         {
-            Console.WriteLine("Please try again");
+            numbAttempts++;
+            while (userAnswer != 1300)                                         //while the answer from the user isn't 1300 run the script below
+            {
+                Console.WriteLine("Please try again");
+                int.TryParse(Console.ReadLine(), out userAnswer);           //reads and converts user input and checks if while condition is still false
+                numbAttempts++;                                                //counts up number of attempts needed
+            }
+            Console.WriteLine("Correct! The answer was 1300!");                // once while loop is complete shows user the correct answer
+            Console.WriteLine($"You guessed Correctly after {numbAttempts} guesses!");
+        }
+        else {                                                                 //if User input could not be converted into string -> ends program and shows no number was entered
+            Console.WriteLine("No Number was entered.");
         }
 
-
-    }
-      
     }
 }
