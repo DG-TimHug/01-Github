81d9b81e826b6deaa5dd4a873c25e750e32bfe06
Tim <tim.hug@digitecgalaxus.ch>
2025-08-13 16:57:14 +0200

    Methoden Aufgabe 3 Fertig

--- 06 Object Oriented Basis/06 Object Oriented Basis/06 Object Oriented Basis/Program.cs
+++ 06 Object Oriented Basis/06 Object Oriented Basis/06 Object Oriented Basis/Program.cs
@@ -1,16 +1,68 @@
-﻿namespace _06_Object_Oriented_Basis;
+﻿using System.Runtime.InteropServices.JavaScript;
+
+namespace _06_Object_Oriented_Basis;
 
 class Program
 {
     static void Main(string[] args)
     {
-      
+        /*
+        //Methoden Aufgabe 1
+        void PrintHelloWorld()
+        {
+            Console.WriteLine("Hello World!");
+        }
+        PrintHelloWorld();
+                                        */
+        /*
+        //Methoden Aufgabe 2
+        int userNum1 = 0;
+        int userNum2 = 0;
+
+        Console.WriteLine("Please enter 2 numbers.");
+        int.TryParse(Console.ReadLine(), out userNum1);
+        int.TryParse(Console.ReadLine(), out userNum2);
+  
+        bool NumberChecker(int userNum1, int userNum2)
+        {
+            if(userNum1 > userNum2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        Console.WriteLine(NumberChecker(userNum1, userNum2));
+                                        */
         
-        
-        
-        
-        
-        
-        
+        //Methoden Aufgabe 3
+        /*
+        Console.WriteLine("Please write a short sentence.");
+        var userInput = Console.ReadLine();
+
+        var output = GapCounter(userInput);
+       Console.WriteLine(output);
     }
+
+    private static int GapCounter(string userInput)
+    {
+        var amountSpaces = 0;
+        foreach (var singleCharacter in userInput)
+        {
+
+            if (singleCharacter == ' ')
+            {
+                amountSpaces++;
+            }
+
+        }
+
+        return amountSpaces;
+                                    
+    }
+                                    */
+    }                
 }
