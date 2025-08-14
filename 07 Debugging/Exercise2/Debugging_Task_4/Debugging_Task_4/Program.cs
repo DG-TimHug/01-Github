3c41f4ca9964bd57ea2b394a543a64d4b1b87316
Tim <tim.hug@digitecgalaxus.ch>
2025-08-14 17:14:14 +0200

    Debugging Task 4 Complete

--- 07 Debugging/Exercise2/Debugging_Task_4/Debugging_Task_4/Program.cs
+++ 07 Debugging/Exercise2/Debugging_Task_4/Debugging_Task_4/Program.cs
@@ -5,20 +5,21 @@ namespace Debugging_Task_4
     class Program
     {
         //gewünschtes Verhalten:es wird 6 und  9 in die Konsole geschrieben
-        private const int numberOfApples = 1;
-        static void Main(string[] args)
+        private static int NumberOfApples = 1;
+
+        private static void Main(string[] args)
         {
             //You get 5 new apples, add them to the existing apples
             AddNumberOfApples(5);
-            Console.WriteLine(numberOfApples);
+            Console.WriteLine(NumberOfApples);
             // You get 3 more, add them too
             AddNumberOfApples(3);
-            Console.WriteLine(numberOfApples);
+            Console.WriteLine(NumberOfApples);
         }
 
-        static int AddNumberOfApples(int numberOfNewApples)
+        private static int AddNumberOfApples(int numberOfNewApples)
         {
-            return numberOfApples + numberOfNewApples;
+            return NumberOfApples += numberOfNewApples;
         }
     }
 }
