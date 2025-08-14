61f1cee3c037818d610126adf9f7a637efd5c952
Tim <tim.hug@digitecgalaxus.ch>
2025-08-14 08:59:23 +0200

    Start of Classes Questions

--- 06 Object Oriented Basis/06 Object Oriented Basis/06 Object Oriented Basis/Program.cs
+++ 06 Object Oriented Basis/06 Object Oriented Basis/06 Object Oriented Basis/Program.cs
@@ -22,7 +22,7 @@ class Program
         Console.WriteLine("Please enter 2 numbers.");
         int.TryParse(Console.ReadLine(), out userNum1);
         int.TryParse(Console.ReadLine(), out userNum2);
-  
+
         bool NumberChecker(int userNum1, int userNum2)
         {
             if(userNum1 > userNum2)
@@ -37,7 +37,6 @@ class Program
 
         Console.WriteLine(NumberChecker(userNum1, userNum2));
                                         */
-        
         //Methoden Aufgabe 3
         /*
         Console.WriteLine("Please write a short sentence.");
@@ -48,7 +47,7 @@ class Program
     }
 
     private static int GapCounter(string userInput)
-    {
+        {
         var amountSpaces = 0;
         foreach (var singleCharacter in userInput)
         {
@@ -61,8 +60,36 @@ class Program
         }
 
         return amountSpaces;
-                                    
-    }
+
+        }
                                     */
+        //Klassen Aufgabe 3 (Aufgaben 1/2 bereits während Recherche gelöst. Siehe Conf für mehr info
+        Console.WriteLine("Enter any number that you want to Subtract from or add to the number 17");
+        int.TryParse(Console.ReadLine(), out int userInput);
+
+
+
+
+
+    }
+    
+
+    public class Counter
+       {
+           public int Count { get; set; }
+
+           private static int Addition(int userInput)
+           {
+               var resultAddition = userInput + 17;
+               return resultAddition;
+           }
+
+           private static int Subtraction(int userInput)
+           {
+               var resultSubtraction = userInput - 17;
+               return resultSubtraction;
+           }
+       }
+        
     }                
-}
+
