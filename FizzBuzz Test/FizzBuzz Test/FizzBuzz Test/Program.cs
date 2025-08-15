828cb64a64f300d29e5b8cf0fc32a8cfc27556fc
Tim <tim.hug@digitecgalaxus.ch>
2025-08-15 15:54:10 +0200

    Final Version FizzBuzz Simple/Complex

--- FizzBuzz Test/FizzBuzz Test/FizzBuzz Test/Program.cs
+++ FizzBuzz Test/FizzBuzz Test/FizzBuzz Test/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
+        //FizzBuzzSimple();
+        //FizzBuzzComplex();
+    }
+
+    private static void FizzBuzzComplex()
+    {
+        for (int number = 1; number <=100; number++)
+        {
+            var output = "";
+            if (number % 3 == 0)
+            {
+                output += "Fizz";
+            }
+            if (number % 5 == 0)
+            {
+                output += "Buzz";
+            }
+            Console.WriteLine(output == "" ? number : output );
+        }
+    }
+
+    private static void FizzBuzzSimple()
+    {
         for (int number = 1; number <=100; number++)
         {
             if (number % 3 == 0 && number % 5 == 0)
