dfa2b122a8c91a14116972d40921606b21634042
Tim <tim.hug@digitecgalaxus.ch>
2025-08-18 08:20:28 +0200

    Initial Release Calculator

---
+++ Calculator Test/Calculator Test/Calculator Test/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Calculator_Test;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello and Welcome to the Calculator.");
+        int selectedOperator = SelectOperator();
+
+        switch (selectedOperator)
+        {
+            case 1:
+        }
+
+    }
+
+    private static int SelectOperator()
+    {
+        Console.WriteLine("Please enter your operator");
+        Console.WriteLine("1 = Addition");
+        Console.WriteLine("2 = Subtraction");
+        Console.WriteLine("3 = Multiplication");
+        Console.WriteLine("4 = Division");
+        Console.WriteLine("5 = Modulo");
+        //Console.WriteLine("6 = View Current result in counter");
+        return SelectOperator();
+    }
+    
+}
