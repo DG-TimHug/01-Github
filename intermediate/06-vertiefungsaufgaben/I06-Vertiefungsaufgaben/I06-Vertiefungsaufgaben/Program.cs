633218d218153e711f0c463b1683debd36714da7
Tim <tim.hug@digitecgalaxus.ch>
2025-09-01 17:00:59 +0200

    Update Program.cs

--- intermediate/06-vertiefungsaufgaben/I06-Vertiefungsaufgaben/I06-Vertiefungsaufgaben/Program.cs
+++ intermediate/06-vertiefungsaufgaben/I06-Vertiefungsaufgaben/I06-Vertiefungsaufgaben/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        var multidimensioanlarray = new bool[6, 9]
+        {
+            { false, false, false, true, true, true, false, false, false }, 
+            { false, false, false, true, true, true, false, false, false }, 
+            { true, true, true, true, true, true, true, true, true,},
+            { true, true, true, true, true, true, true, true, true,},
+            { false, false, false, true, true, true, false, false, false }, 
+            { false, false, false, true, true, true, false, false, false },
+        };
+        for (int line = 0; line <= 6; line++)
+        {
+            foreach (var State in multidimensioanlarray)
+            {
+
+                if (State == true)
+                {
+                    Console.Write("X, ");
+                }
+                else
+                {
+                    Console.Write(" , ");
+                }
+            }
+            Console.WriteLine();
+        }
     }
 }
