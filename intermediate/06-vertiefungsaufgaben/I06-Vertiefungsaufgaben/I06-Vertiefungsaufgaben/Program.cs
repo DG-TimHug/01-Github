06bc760ffe92eff275cd7a5697b6a0b1d81b87cb
Tim <tim.hug@digitecgalaxus.ch>
2025-09-02 12:29:40 +0200

    Update Program.cs

--- intermediate/06-vertiefungsaufgaben/I06-Vertiefungsaufgaben/I06-Vertiefungsaufgaben/Program.cs
+++ intermediate/06-vertiefungsaufgaben/I06-Vertiefungsaufgaben/I06-Vertiefungsaufgaben/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     private static void Main()
     {
-        var cross = new bool[6, 9]
+        var cross = new bool[,]
         {
             { false, false, false, true, true, true, false, false, false }, 
             { false, false, false, true, true, true, false, false, false }, 
