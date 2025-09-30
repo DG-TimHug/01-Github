a4c12e8ee673433b4815dd7c1031c0917ef322f2
Tim <tim.hug@digitecgalaxus.ch>
2025-09-30 16:59:24 +0200

    Update Program.cs

--- advanced/a01-Collections/a01-Collections/Program.cs
+++ advanced/a01-Collections/a01-Collections/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+       /*
+        * Listen sind wesentlich flexibler wie Arrays, da man wann man will daten hinzufügen, abrufen, verändern und Löschen kann.
+        */
     }
 }
