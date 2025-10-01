2a27682fd495d4918147a0076d5831cc01e1d286
Tim <tim.hug@digitecgalaxus.ch>
2025-10-01 11:33:48 +0200

    lunch :)

--- advanced/a01-Collections/a01-Collections/Program.cs
+++ advanced/a01-Collections/a01-Collections/Program.cs
@@ -6,6 +6,14 @@ class Program
     {
        /*
         * Listen sind wesentlich flexibler wie Arrays, da man wann man will daten hinzufügen, abrufen, verändern und Löschen kann.
+        *
+        * Unterschiede zwischen Sets und Listen. Listen sind Geordnete Orte wo ein Wert mehrmals vorkommen kann vs. ein Set ein Ungeordneter Ort ist wo ein Wert nur einmal vorkommen kann.
         */
+       List<int> list = new List<int>();
+       
+       list.Add(10);
+       list.Add(25);
+       list.Add(15);
+       list.Add(10);
     }
 }
