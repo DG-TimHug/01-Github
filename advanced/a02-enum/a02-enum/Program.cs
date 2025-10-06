ede54cd2080d234dfcd70df67c0ef8e285c29a77
Tim <tim.hug@digitecgalaxus.ch>
2025-10-06 11:33:54 +0200

    lunch

--- advanced/a02-enum/a02-enum/Program.cs
+++ advanced/a02-enum/a02-enum/Program.cs
@@ -2,8 +2,25 @@
 
 class Program
 {
+    enum Gender
+    {
+        Male,
+        Female,
+        Unknown
+    }
     static void Main(string[] args)
     {
-        //
+        
+    }
+
+    private class Person
+    {
+        private string firstname;
+        private string lastname;
+        private int age; 
+        static object Gender { get; set; }
+        private Gender mygender;
+        private Person tim = new Person() { firstname = "Tim", lastname = "Hug", age = 15, mygender = Program.Gender.Male };
+
     }
 }
