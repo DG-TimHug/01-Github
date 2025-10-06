093de6fd8233c2bcba43ac02aae39d0727b5e4ea
Tim <tim.hug@digitecgalaxus.ch>
2025-10-06 12:47:43 +0200

    Queestion 1 complete

--- advanced/a02-enum/a02-enum/Program.cs
+++ advanced/a02-enum/a02-enum/Program.cs
@@ -2,25 +2,8 @@
 
 class Program
 {
-    enum Gender
-    {
-        Male,
-        Female,
-        Unknown
-    }
     static void Main(string[] args)
     {
         
-    }
-
-    private class Person
-    {
-        private string firstname;
-        private string lastname;
-        private int age; 
-        static object Gender { get; set; }
-        private Gender mygender;
-        private Person tim = new Person() { firstname = "Tim", lastname = "Hug", age = 15, mygender = Program.Gender.Male };
-
     }
 }
--- advanced/a02-enum/a02-enum/Question1.cs
+++ advanced/a02-enum/a02-enum/Question1.cs
@@ -2,5 +2,20 @@ namespace a02_enum;
 
 public class Question1
 {
-    //
+    
+}
+ public enum Gender
+   {
+       Male,
+       Female,
+       Unknown
+   }
+
+class Person
+{
+    private string firstname;
+    private string lastname;
+    private int age;
+    private Gender Gender;
+    private Person tim = new Person() { firstname = "Tim", lastname = "Hug", age = 15, Gender = Gender.Male };
 }
