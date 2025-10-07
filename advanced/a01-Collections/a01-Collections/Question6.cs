cc8a396aafc9eb7435f7d3479451fb5df5b8caa8
Tim <tim.hug@digitecgalaxus.ch>
2025-10-07 08:51:51 +0200

    Fixed PR comments.
    Moved main parts to new Method ExcahngerateCalculator and moved each currency variable to respective case

--- advanced/a01-Collections/a01-Collections/Question6.cs
+++ advanced/a01-Collections/a01-Collections/Question6.cs
@@ -4,13 +4,11 @@ public class Question6
 {
     public static void ExchangeCourse()
     {
-        Dictionary<string, double> exchangeRatesToChfByCurrency = new Dictionary<string?, double>();
+        Dictionary<string, double> exchangeRatesToChfByCurrency = new Dictionary<string, double>();
 
         exchangeRatesToChfByCurrency.Add("CHF", 1.0);
         exchangeRatesToChfByCurrency.Add("EUR", 0.94);
         exchangeRatesToChfByCurrency.Add("USD", 0.80);
-        var eurToChf = exchangeRatesToChfByCurrency["EUR"];
-        var usdToChf = exchangeRatesToChfByCurrency["USD"];
         var chfToChf = exchangeRatesToChfByCurrency["CHF"];
         
         Console.WriteLine("From which Currency do you want to trade from? (ex. EUR, CHF, USD)");
@@ -23,38 +21,28 @@ public class Question6
                 case "EUR":
                 {
                     var currentCurrency = "EUR";
-                    Console.WriteLine("How much would you like to trade?");
-                    double.TryParse(Console.ReadLine(), out var inputAmount);
-                    var outInChf = eurToChf * inputAmount;
-                    var outputAmountInChf = Math.Round(outInChf, 2);
-                    Console.WriteLine($"That turns into {outputAmountInChf} CHF at an exchange rate of {eurToChf} {currentCurrency} to {chfToChf} CHF.");
+                    var eurToChf = exchangeRatesToChfByCurrency["EUR"];
+                    ExchangeRateCalculator(eurToChf, chfToChf, currentCurrency);
                     break;
                 }
 
                 case "USD":
                 {
                     var currentCurrency = "USD";
-                    Console.WriteLine("How much would you like to trade?");
-                    double.TryParse(Console.ReadLine(), out var inputAmount);
-                    var outInChf = usdToChf * inputAmount;
-                    var outputAmountInChf = Math.Round(outInChf, 2);
-                    Console.WriteLine($"That turns into {outputAmountInChf} CHF at an exchange rate of {usdToChf} {currentCurrency} to {chfToChf} CHF.");
+                    var usdToChf = exchangeRatesToChfByCurrency["USD"];
+                    ExchangeRateCalculator(usdToChf, chfToChf, currentCurrency);
                     break;
                 }
 
                 case "CHF":
                 {
                     var currentCurrency = "CHF";
-                    Console.WriteLine("How much would you like to trade?");
-                    double.TryParse(Console.ReadLine(), out var inputAmount);
-                    var outInChf = chfToChf * inputAmount;
-                    var outputAmountInChf = Math.Round(outInChf, 2);
-                    Console.WriteLine($"That turns into {outputAmountInChf} CHF at an exchange rate of {chfToChf} {currentCurrency} to {chfToChf} CHF.");
+                    ExchangeRateCalculator(chfToChf, chfToChf, currentCurrency);
                     break;
                 }
                 default:
                 {
-                    throw new Exception();
+                    throw new ArgumentException();
                 }
             }
         }
@@ -62,5 +50,14 @@ public class Question6
         {
             Console.WriteLine("Currency was not Found!");
         }
+    }
+
+    private static void ExchangeRateCalculator(double currency, double swissFranc, string currentCurrency)
+    {
+        Console.WriteLine("How much would you like to trade?");
+        double.TryParse(Console.ReadLine(), out var inputAmount);
+        var outInChf = currency * inputAmount;
+        var outputAmountInChf = Math.Round(outInChf, 2);
+        Console.WriteLine($"That turns into {outputAmountInChf} CHF at an exchange rate of {currency} {currentCurrency} to {swissFranc} CHF.");
     }
 }
