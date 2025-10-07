ba4faff71391e89117f1130ad36b745460e5fb70
Tim <tim.hug@digitecgalaxus.ch>
2025-10-07 10:19:33 +0200

    Question 1 complete Start Question2

---
+++ advanced/a04-Linq/a04-Linq/Question1.cs
@@ -0,0 +1,16 @@
+namespace a04_Linq;
+
+public class Question1
+{
+    public static void Execute()
+    {
+        int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+
+        IEnumerable<int> numQuery2 = numbers.Where(num => num % 3 == 0).OrderBy(n => n);
+
+        foreach (int i in numQuery2)
+        {
+            Console.WriteLine(i + " ");
+        }
+    }
+}
---
+++ advanced/a04-Linq/a04-Linq/Question2.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace a04_Linq;
+
+public class Question2
+{
+    public static void Execute()
+    {
+        int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+
+        IEnumerable<int> numQuery2 = numbers.Where(num => num % 3 == 0).OrderBy(n => n);
+        var result = 0;
+        foreach (int i in numQuery2)
+        {
+            result += i;
+        }
+        Console.WriteLine(result);
+    }
+}
