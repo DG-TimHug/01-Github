308bec2fd2911d4ff61f945a51c4345ef53509bc
Tim <tim.hug@digitecgalaxus.ch>
2025-10-20 14:01:15 +0200

    temp Commit

--- advanced/a06.Interfaces/a06.Interfaces/Program.cs
+++ advanced/a06.Interfaces/a06.Interfaces/Program.cs
@@ -1,9 +1,56 @@
-﻿namespace a06.Interfaces;
+﻿using System.Net.Mime;
+
+namespace a06.Interfaces;
 
 class Program
 {
     static void Main()
     {
+        SimpleConsoleWriter writeText = new SimpleConsoleWriter();
+        writeText.Print("Bobby Lee");
+        writeText.PrintLine("idk");
+        writeText.PrintLine();
+        writeText.PrintLine(SomeNumbers());
+    }
 
+    public interface ITextWriter
+    {
+        void Print(string Text);
+
+
+        void PrintLine(string Text);
+
+        void PrintLine();
+    }
+
+    public class SimpleConsoleWriter : ITextWriter
+    {
+        public void Print(string Text)
+        {
+            Console.WriteLine(Text);
+        }
+
+        public void PrintLine(string Text)
+        {
+            Console.WriteLine(Text);
+        }
+
+        public void PrintLine()
+        {
+           
+        }
+    }
+
+    public class RandomTextWriter
+    {
+        public TextWriter ITextWriter { get; set; }
+
+        public void SomeNumbers()
+        {
+            for (int numbers = 0; numbers < 10; numbers++)
+            {
+                
+            }
+        }
     }
 }
