6a8520bccfbf8096dcb700f2847a21fc62ed9436
Tim <tim.hug@digitecgalaxus.ch>
2025-10-20 08:55:05 +0200

    temp commit

--- advanced/a05.Vererbung-AbstrakteKlassen/a05.Vererbung-AbstrakteKlassen/Program.cs
+++ advanced/a05.Vererbung-AbstrakteKlassen/a05.Vererbung-AbstrakteKlassen/Program.cs
@@ -2,8 +2,62 @@
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
+
+    
+    }
+    public class Fahrzeug
+    {
+        public int Speed { get; protected set; }
         
+        public int Gear { get; protected set; }
+        
+        public string BlinkerSetting {get; protected set;}
+        
+        public string Owner { get; protected set; }
+        
+        public int Buildyear { get; protected set; }
+        
+        public int Tankvolume { get; protected set; }
+        
+        public int TankContent { get; protected set; }
+        
+        public void Accelerate()
+        {
+            this.Speed++;
+        }
+
+        public void Decelerate()
+        {
+            if (this.Speed > 0)
+            {
+                this.Speed--;
+            }
+        }
+
+        public void GearShifterUp(int Gear)
+        {
+            if (Gear >= 8)
+            {
+                
+            }
+            else
+            {
+                Gear++;
+            }
+        }
+
+        public void GearShifterDown(int Gear)
+        {
+            if (Gear <= -1)
+            {
+                
+            }
+            else
+            {
+                Gear--;
+            }
+        }
     }
 }
